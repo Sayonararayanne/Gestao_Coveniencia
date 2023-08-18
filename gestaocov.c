f4c44ffb3083150d004bf1e5059780425257f85d
Sayonararayanne <109805650+Sayonararayanne@users.noreply.github.com>
2023-08-18 20:16:45 -0300

    Criação do menuprincipal

--- gestaocov.c
+++ gestaocov.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void menuop (void);
+void menuprincipal (void);
 
 int main (void){
 menuop();
+menuprincipal();
 return 0; 
 }
 
@@ -20,4 +22,20 @@ void menuop (void){
     printf("------------------------------------------------\n");
 
 }
+
+void menuprincipal (void){
+    printf("------------------------------------------------\n");
+    printf("-------- GESTÃO PARA LOJA DE COVENIÊNCIA -------\n");
+    printf("--------------- MENU PRINCIPAL -----------------\n");
+    printf("------------------------------------------------\n");
+    printf("\n");
+    printf("(1) - MÓDULO DE PRODUTOS\n");
+    printf("(2) - MÓDULO DE VENDAS\n");
+    printf("(3) - MÓDULO DE CLIENTES\n");
+    printf("(4) - MÓDULO DE FUNCIONÁRIOS\n");
+    printf("(0) - SAIR\n");
+    printf("------------------------------------------------\n");
+
+}
+
 
