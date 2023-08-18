971abd3a769f92b71a09acfec0b3223ca96b3f22
Sayonararayanne <109805650+Sayonararayanne@users.noreply.github.com>
2023-08-18 20:10:50 -0300

    Criação do menuop (menu opções)

---
+++ gestaocov.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void menuop (void);
+
+int main (void){
+menuop();
+return 0; 
+}
+
+void menuop (void){ 
+    printf("------------------------------------------------\n");
+    printf("-------- GESTÃO PARA LOJA DE COVENIÊNCIA -------\n");
+    printf("-------------- SEJA BEM - VINDO ----------------\n");
+    printf("------------------------------------------------\n");
+    printf("\n");
+    printf("(1) - MENU PRINCIPAL\n");
+    printf("(2) - SOBRE\n");
+    printf("(3) - EQUIPE\n");
+    printf("(0) - SAIR\n");
+    printf("------------------------------------------------\n");
+
+}
+
